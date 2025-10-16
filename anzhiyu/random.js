82c35e3c58cee33a930e27fba522c687668a0d40
Karovia <luopeilindeqq123@foxmail.com>
2025-10-16 15:16:42 +0800

    Site updated: 2025-10-16 15:16:41

---
+++ anzhiyu/random.js
@@ -0,0 +1,3 @@
+var posts=["2025/10/16/Linux命令/","2025/10/16/Typora使用指南（Markdown语法）/","2025/10/16/代理和谷歌账号注册/","2025/10/16/Win11 安装 WSL 和 Ubuntu时踩的坑/","2025/10/16/git的使用/"];function toRandomPost(){
+    pjax.loadUrl('/'+posts[Math.floor(Math.random() * posts.length)]);
+  };
